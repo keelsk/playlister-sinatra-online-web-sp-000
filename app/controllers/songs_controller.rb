b4759fb2b6b0e6319cc6b013b1feb3e06a1acea4
Kirsten Keels <keelsk@gmail.com>
2020-04-03 23:53:03 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -35,7 +35,7 @@ class SongsController < ApplicationController
     @new_song.genres << Genre.find_or_create_by(params[:genre]) if !params[:genre][:name].empty?
     @new_song.save
     flash[:message] = "Successfully updated song."
-    redirect to("/songs/#{@new_song.slug}")
+    redirect "/songs/#{@new_song.slug}"
   end
 
   get '/songs/:slug' do
