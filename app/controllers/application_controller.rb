bebe3defd355abbb12ff18219a439d9d55bfde78
Kirsten Keels <keelsk@gmail.com>
2020-04-03 23:57:41 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ require 'rack-flash'
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   enable :sessions
-  use Rack::Flash, :sweep => true
+  use Rack::Flash
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
