985d20a7a63d89a2cac3a02217851367cb9c0aaf
Kirsten Keels <keelsk@gmail.com>
2020-04-04 00:27:47 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -28,7 +28,7 @@ class SongsController < ApplicationController
     erb :'songs/edit'
   end
 
-  patch '/songs' do
+  patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     @song = @song.update(params[:song])
     @song.artist = Artist.find_or_create_by(params[:artist]) if !params[:artist][:name].empty?
