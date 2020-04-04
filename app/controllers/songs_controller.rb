f91c26a271ac8d4b398614ef6b37a3fb484dda26
Kirsten Keels <keelsk@gmail.com>
2020-04-04 00:23:08 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -17,7 +17,7 @@ class SongsController < ApplicationController
     @song.artist = Artist.find_or_create_by(params[:artist]) if !params[:artist][:name].empty?
     @song.genres << Genre.find_or_create_by(params[:genre]) if !params[:genre][:name].empty?
     @song.save
-    @message = "Successfully created song."
+    flash[:message] = "Successfully created song."
     redirect "/songs/#{@song.slug}"
   end
 
@@ -34,7 +34,7 @@ class SongsController < ApplicationController
     @song.artist = Artist.find_or_create_by(params[:artist]) if !params[:artist][:name].empty?
     @song.genres << Genre.find_or_create_by(params[:genre]) if !params[:genre][:name].empty?
     @song.save
-    @message = "Successfully updated song."
+    flash[:message].next = "Successfully updated song."
     redirect "/songs/#{@song.slug}"
   end
 
@@ -42,7 +42,6 @@ class SongsController < ApplicationController
     @song = Song.find_by_slug(params[:slug])
     @artist = @song.artist
     @genres = @song.genres
-    flash[:message] = @message
     erb :'songs/show'
   end
 
